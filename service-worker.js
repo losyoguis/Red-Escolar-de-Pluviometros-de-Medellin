ef2c578ee0b2459002f91e3257a6c06131492de3
Juan Carlos Blandon Vargas <yoguisindevoz@gmail.com>
2026-04-25 14:52:31 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'red-pluviometros-medellin-v4-6-backend-configurado';
+const CACHE_NAME = 'red-pluviometros-medellin-v4-8-mapa-ultima-foto';
 const ASSETS = [
   './', './index.html', './data/instituciones.js', './assets/logo_losyoguis.png', './assets/footer_losyoguis.png', './manifest.webmanifest'
 ];
